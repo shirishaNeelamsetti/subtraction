364b835e8dcf6804763da7702163be78e53928f9
shirishaNeelamsetti <shirishaneelamsetti04@gmail.com>
2024-09-28 13:28:21 +0530

    Add files via upload
    commit in first

---
+++ src/main/java/com/arthematic_operations/Subtraction.java
@@ -0,0 +1,12 @@
+package com.arthematic_operations;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+int a=10;
+int b=5;
+int c=a-b;
+System.out.println("subtraction of 2 integers are :"+c);
+	}
+
+}
